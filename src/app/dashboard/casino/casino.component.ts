7817969e9c48ca680ea80df9f01e411ba1506372
sus589 <susmitadas.ei@gmail.com>
2026-02-02 08:26:25 +0530

    done

--- src/app/dashboard/casino/casino.component.ts
+++ src/app/dashboard/casino/casino.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { SidebarService } from '../../sidebar.service';
 import { LeftSidebarComponent } from '../../left-sidebar/left-sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-casino',
-  imports: [LeftSidebarComponent],
+  imports: [LeftSidebarComponent, CommonModule],
   templateUrl: './casino.component.html',
   styleUrl: './casino.component.css'
 })
 export class CasinoComponent {
   isSidebarOpen = false;
+
+  constructor(private sidebarService: SidebarService) {
+    this.sidebarService.sidebarOpen$
+      .subscribe(state => this.isSidebarOpen = state);
+  }
+
+  activeTab: string = "ALL";
+
+  changeTab(tab: any) {
+    this.activeTab = tab;
+  }
   
-    constructor(private sidebarService: SidebarService) {
-      this.sidebarService.sidebarOpen$
-        .subscribe(state => this.isSidebarOpen = state);
-    }
+  isOpenSearchPage: boolean = false;
+  openSearchPage() {
+    this.isOpenSearchPage = true;
+  }
+  closeSearchPage() {
+    this.isOpenSearchPage = false;
+  }
 }
